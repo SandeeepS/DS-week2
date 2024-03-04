02cbe672234f98e9fc52ec791e78f4f7526d2fcc
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-03-04 15:45:28 +0530

    test files is changed

--- test.js
+++ test.js
@@ -1,69 +1,81 @@
-class HashTable {
-     constructor(size){
-         this.table = new Array(size);
-         this.size = size;
+//priority queue using linkedlist 
+
+class Node {
+     constructor(value,priority){
+        this.value = value;
+        this.priority = priority;
+        this.next = null;
+     }
+}
+
+class PriorityQueue{
+     constructor() {
+           this.head = null;
+           this.tail = null;
+           this.size = 0;
      }
 
-     hash(key){
-        let total = 0;
-        for(let i = 0 ; i < key.length ; i++){
-            total += key.charCodeAt(i);
+     isEmpty(){
+        return this.size === 0 
+     }
+     enqueue(element, priority) {
+        const newNode = new Node(element, priority);
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            let current = this.head;
+            let prev = null;
+            while (current && current.priority <= priority) {
+                prev = current;
+                current = current.next;
+            }
+            if (!prev) {
+                newNode.next = this.head;
+                this.head = newNode;
+            } else {
+                newNode.next = current;
+                prev.next = newNode;
+            }
+            if (!current) {
+                this.tail = newNode;
+            }
         }
-        return total % this.size ;
-     }
+        this.size++;
+    }
 
-     set(key,value){
-        let index = this.hash(key);
-        const bucket = this.table[index];
-        if(!bucket){
-            this.table[index] = [[key,value]];
+    dequeue(){
+        if(this.isEmpty()){
+            console.log("the queue is empty");
+        }
+           
+        const elem = this.head.value;
+        this.head = this.head.next;
+        this.size--;
+        return elem;
+    }
+
+     print(){
+        if(this.isEmpty()){
+            console.log("the queue is empty");
         }else{
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if(sameKeyItem){
-                sameKeyItem[1] = value;
-            }else{
-                bucket.push([key,value]);
+            let prev = this.head;
+            let elements = '';
+            while(prev){
+                elements += `${prev.value} `;
+                prev = prev.next;
             }
-        }
-     }
 
-     get(key){
-        let index = this.hash(key);
-        const bucket = this.table[index];
-        if(bucket){
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if(sameKeyItem){
-                return sameKeyItem[1];
-            }
-        }
-        return undefined;
-     }
-
-     remove(key){
-        let index = this.hash(key);
-        const bucket = this.table[index];
-        if(bucket){
-            const sameKeyItem = bucket.find(item => item[0] === key);
-            if(sameKeyItem){
-                bucket.splice(bucket.indexOf(sameKeyItem),1);
-            }
-        }
-     }
-
-     display(){
-        for(let i = 0 ; i < this.table.length ; i++){
-            if(this.table[i]){
-                console.log(i,this.table[i]);
-            }
+            console.log(elements);
         }
      }
 }
 
-const hashTable = new HashTable(50);
-hashTable.hash("hello");
-hashTable.set("hello",456);
-hashTable.set(45,789);
-hashTable.set(46,800);
-hashTable.remove(46);
-console.log(hashTable.get(46));
-hashTable.display();
+const queue = new PriorityQueue();
+queue.enqueue(10,1);
+queue.enqueue(20,2);
+queue.enqueue(30,3);
+queue.enqueue(60,2);
+queue.print();
+console.log(queue.dequeue());
+queue.print();
