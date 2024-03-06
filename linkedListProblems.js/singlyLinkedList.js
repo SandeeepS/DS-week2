18f8906e62949dda34fe8153b6ae97b851e8b2e4
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-03-06 18:12:52 +0530

    added singly linked list

---
+++ linkedListProblems.js/singlyLinkedList.js
@@ -0,0 +1,92 @@
+//singly list problem 
+
+class Node{
+    constructor(value){
+        this.value = value;
+        this.next  = null;
+    }
+}
+
+class linkedList{
+    constructor(){
+        this.head = null;
+        this.size = 0;
+    }
+
+    isEmpty(){
+        return this.size === 0
+    }
+
+    peek(){
+        console.log(this.head.value);
+    }
+
+    prepend(value){
+        let node = new Node(value);
+        if(this.isEmpty()){
+            this.head = node;
+        }else{
+            node.next = this.head;
+            this.head = node;
+        }
+        this.size ++;
+    }
+
+    append(value){
+        let node = new Node(value);
+        if(this.isEmpty()){
+             this.head = node;
+        }else{
+            let prev = this.head;
+            while(prev && prev.next != null){
+                prev = prev.next;
+            }
+            prev.next = node;
+        }
+        this.size ++;
+    }
+
+    print(){
+        if(this.isEmpty()){
+            console.log("list is empty");
+            return;
+        }
+        let prev = this.head;
+        let element = '';
+        while(prev){
+            element += `${prev.value} `;
+            prev = prev.next;
+        }
+        console.log(element);
+    }
+
+    removeByValue(value){
+        if(this.isEmpty()){
+            console.log("the list is empty");
+            return ;
+        }
+        
+        if(this.head.value == value){
+            console.log(0);
+        }else{
+            let prev = this.head;
+            let i = 0;
+            while(prev){
+                if(prev.value == value){
+                    console.log(i)
+                }
+                prev = prev.next;
+                i++;
+            }
+        }
+    }
+}
+
+let linkedList1 = new linkedList();
+linkedList1.prepend(10);
+linkedList1.prepend(20);
+linkedList1.prepend(30);
+linkedList1.append(100);
+linkedList1.append(120);
+linkedList1.print();
+linkedList1.removeByValue(20);
